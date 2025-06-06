bcd9d65ab967f3badb7e4c774d9995fbe8b56ce0
Enes Gençer <enes.gencer0638@gmail.com>
2025-06-06 22:47:36 +0300

    Class oluşturma

---
+++ Hunkar_Sofrasi/Hunkar_Sofrasi/cVeriTabani.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunkar_Sofrasi
+{
+    internal class cVeriTabani
+    {
+        public string conString = "Server=DESKTOP-49A315Q\\Enes;Database=HUNKARSOFRASI;Trusted_Connection=true";
+
+
+    }
+}
